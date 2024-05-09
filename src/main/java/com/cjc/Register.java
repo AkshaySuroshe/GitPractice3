2725e8892b41c4e8320e144ab84d1f387c290e09
AkshaySuroshe <akshaysuroshe4@gmail.com>
2024-05-09 16:13:15 +0530

    Sukuu changes

--- src/main/java/com/cjc/Register.java
+++ src/main/java/com/cjc/Register.java
@@ -8,3 +8,15 @@ class
   System.out.println("pulling main branch to login branch");  
   System.out.println("pulling main branch to login branch");  
   System.out.println("pulling main branch to login branch");
+
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+  System.out.println("Sukuu");
+
+  
