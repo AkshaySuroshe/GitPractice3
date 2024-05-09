9556434f48494066e6deaaebef9af2f80666561f
AkshaySuroshe <akshaysuroshe4@gmail.com>
2024-05-09 16:08:57 +0530

    Uodate println()

--- src/main/java/com/cjc/Register.java
+++ src/main/java/com/cjc/Register.java
@@ -1,2 +1,3 @@
 class
   System.out.println("pulling main branch to login branch");
+  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");  System.out.println("pulling main branch to login branch");
